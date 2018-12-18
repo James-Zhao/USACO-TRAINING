61ff611a7ecdbdbff539cf38fd1f88ec3836cb54
James-Zhao <jamezhaosi@163.com>
2018-12-18 13:24:15 +0800

    format

--- Java/src/section1_2/ride.java
+++ Java/src/section1_2/ride.java
@@ -9,7 +9,7 @@ import java.util.*;
 public class ride {
     public static void main (String [] args) throws IOException {
         // Use BufferedReader rather than RandomAccessFile; it's much faster
-        BufferedReader f = new BufferedReader(new FileReader("ride.in"));
+        BufferedReader f = new BufferedReader(new FileReader("test.in"));
         // input file name goes above
         PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("ride.out")));
 
--- Java/src/section1_3/namenum.java
+++ Java/src/section1_3/namenum.java
@@ -1,3 +1,5 @@
+
+
 import java.io.*;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
